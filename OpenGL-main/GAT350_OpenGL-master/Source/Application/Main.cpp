7820ecf61ebd495ab213951520c7981143be56c7
Cole-Lowery <CLowery@student.neumont.edu>
2025-11-12 09:44:08 -0700

    updates

---
+++ OpenGL-main/GAT350_OpenGL-master/Source/Application/Main.cpp
@@ -0,0 +1,57 @@
+
+int main(int argc, char* argv[]) {
+    neu::file::SetCurrentDirectory("Assets");
+    LOG_INFO("current directory {}", neu::file::GetCurrentDirectory());
+
+    // initialize engine
+    LOG_INFO("initialize engine...");
+    neu::GetEngine().Initialize();
+
+    // initialize scene
+    SDL_Event e;
+    bool quit = false;
+
+    // initialize scene
+    auto scene = std::make_unique<neu::Scene>();
+    scene->Load("scenes/scene01.json");
+    scene->Start();
+
+    auto editor = std::make_unique<neu::Editor>();
+
+    // MAIN LOOP
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
+                quit = true;
+            }
+			ImGui_ImplSDL3_ProcessEvent(&e);
+        }
+
+        // update
+        neu::GetEngine().Update();
+        float dt = neu::GetEngine().GetTime().GetDeltaTime();
+        if (neu::GetEngine().GetInput().GetKeyPressed(SDL_SCANCODE_ESCAPE)) quit = true;
+
+        //scene
+        scene->Update(dt);
+
+        //editor
+        editor->Begin();
+        editor->UpdateGui(*scene);
+
+        // draw
+        neu::GetEngine().GetRenderer().Clear();
+
+        scene->Draw(neu::GetEngine().GetRenderer());
+        
+        // draw ImGui
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        neu::GetEngine().GetRenderer().Present();
+    }
+
+    neu::GetEngine().Shutdown();
+
+    return 0;
+}
---
+++ OpenGL-main/GAT350_OpenGL-master/Source/Engine/EngineMinimal.h
@@ -0,0 +1,92 @@
+#pragma once
+
+// core
+#include "Core/Assert.h"
+#include "Core/Factory.h"
+#include "Core/File.h"
+#include "Core/Json.h"
+#include "Core/Logger.h"
+#include "Core/Random.h"
+#include "Core/Singleton.h"
+#include "Core/StringHelper.h"
+#include "Core/Time.h"
+
+// framework
+#include "Framework/Object.h"
+#include "Framework/Actor.h"
+#include "Framework/Scene.h"
+
+// math
+#include "Math/Math.h"
+#include "Math/Transform.h"
+#include "Math/Vector2.h"
+#include "Math/Vector3.h"
+#include "Math/Rect.h"
+
+// resources
+#include "Resources/Resource.h"
+#include "Resources/ResourceManager.h"
+
+// event manager
+#include "Event/Event.h"
+#include "Event/EventManager.h"
+#include "Event/Observer.h"
+
+//GUI
+#include "GUI/Editor.h"
+#include "GUI/GUI.h"
+
+// input
+#include "Input/InputSystem.h"
+
+// audio
+#include "Audio/AudioClip.h"
+#include "Audio/AudioChannel.h"
+#include "Audio/AudioSystem.h"
+
+// renderer
+#include "Renderer/Renderer.h"
+#include "Renderer/Texture.h"
+#include "Renderer/TextureAnimation.h"
+#include "Renderer/Shader.h"
+#include "Renderer/Program.h"
+#include "Renderer/VertexBuffer.h"
+#include "Renderer/Model.h"
+#include "Renderer/Material.h"
+
+// physics
+#include "Physics/Collidable.h"
+
+// componednts
+#include "Components/ModelRenderer.h"
+#include "Components/CameraComponent.h"
+#include "Components/LightComponent.h"
+#include "Components/FirstPersonController.h"
+
+// third-party library
+#include <fmod.hpp>
+#include <fmod_errors.h>
+
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// standard library
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <vector>
+#include <map>
+#include <list>
+#include <set>
+#include <cstdlib>
+#include <random>
+#include <algorithm>
+#include <cmath>
+#include <filesystem>
+
